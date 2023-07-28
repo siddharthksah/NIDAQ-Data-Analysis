c8d8903152724a5f0a7035e4b0b5ff9f956ba4d5
Siddharth Kumar Sah <siddharth123sk@gmail.com>
2023-07-28 21:59:43 +0800

    Update moving_window_standard_deviation_plot.py

--- moving_window_standard_deviation_plot.py
+++ moving_window_standard_deviation_plot.py
@@ -1,88 +1,126 @@
 """
-This script processes and visualizes the data from the Whiskers Array Sensing project.
-It plots the Moving Window Standard Deviation (MWSD) of Raw Voltage over time for different PSI values.
-The data files are assumed to be in the format with comma-separated values (CSV) where the first column is time and the second column is voltage.
+A script that processes and visualizes data recorded by the National Instrument Data Acquisition (NIDAQ) tool.
+This script reads the data from files, processes them, and generates an animation showing the data trends over time.
 
 Author: Siddharth Kumar (www.siddharthsah.com)
+
+Usage: This script is meant to be run from the command line.
 """
 
+# Standard library imports
 import os
+import logging
+from typing import List, Tuple
+
+# Third party imports
 import matplotlib
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import pandas as pd
-from scipy import signal
 from matplotlib import style
+from scipy import signal
+
+# Setting the appropriate backend before importing pyplot
+matplotlib.use('TkAgg')
 
 # Set the style for the plotting
 style.use('fivethirtyeight')
 
-# Declare the plot figure
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(1,1,1)
+# Configuring the logger
+logging.basicConfig(level=logging.INFO)
 
-# Define the file paths for the data files
-file_paths = [
-    "D:/SMP/OneDrive - Singapore University of Technology and Design/Whiskers Array Sensing/191001/Stiff Whisker 2 cm/10 PSI Sweep_1.txt",
-    "D:/SMP/OneDrive - Singapore University of Technology and Design/Whiskers Array Sensing/191001/Stiff Whisker 2 cm/20 PSI Sweep_1.txt",
-    "D:/SMP/OneDrive - Singapore University of Technology and Design/Whiskers Array Sensing/191001/Stiff Whisker 2 cm/30 PSI Sweep_1.txt",
-    "D:/SMP/OneDrive - Singapore University of Technology and Design/Whiskers Array Sensing/191001/Stiff Whisker 2 cm/40 PSI Sweep_1.txt",
-    "D:/SMP/OneDrive - Singapore University of Technology and Design/Whiskers Array Sensing/191001/Stiff Whisker 2 cm/50 PSI Sweep_1.txt",
-    "D:/SMP/OneDrive - Singapore University of Technology and Design/Whiskers Array Sensing/191001/Stiff Whisker 2 cm/60 PSI Sweep_1.txt",
-    "D:/SMP/OneDrive - Singapore University of Technology and Design/Whiskers Array Sensing/191001/Stiff Whisker 2 cm/70 PSI Sweep_1.txt",
-    "D:/SMP/OneDrive - Singapore University of Technology and Design/Whiskers Array Sensing/191001/Stiff Whisker 2 cm/80 PSI Sweep_1.txt",
-    "D:/SMP/OneDrive - Singapore University of Technology and Design/Whiskers Array Sensing/191001/Stiff Whisker 2 cm/90 PSI Sweep_1.txt",
-    "D:/SMP/OneDrive - Singapore University of Technology and Design/Whiskers Array Sensing/191001/Stiff Whisker 2 cm/100 PSI Sweep_1.txt"
-]
 
-def process_file(file_path):
+def load_data_from_file(file_path: str) -> Tuple[List[float], List[float]]:
     """
-    Function to read and process data file. Extracts time and voltage data. 
+    Load the data from a text file.
 
     Args:
-        file_path: str. Path to the data file.
+        file_path (str): The path to the file.
 
     Returns:
-        Tuple of time and voltage data.
+        Tuple[List[float], List[float]]: A tuple containing two lists:
+            - the time-series data (ts)
+            - the corresponding measurement data (xs)
     """
+    logging.debug(f'Loading data from {file_path}')
     try:
-        with open(file_path, 'r') as file:
-            data = file.read().split('\n')
-
-        ts = []
-        xs = []
-        for line in data:
-            if len(line) > 1:
-                time, voltage = map(float, line.split(','))
-                ts.append(time/2)
-                xs.append(-voltage)
-                if time >= 80:
-                    break
-
-        return ts, pd.Series(xs).rolling(300).std()
-
+        with open(file_path, 'r') as f:
+            data = f.read()
     except FileNotFoundError:
-        print(f"Error: File {file_path} not found.")
+        logging.error(f'{file_path} not found.')
         return [], []
 
-def animate(i):
+    lines = data.split('\n')
+    ts = []
+    xs = []
+
+    for line in lines:
+        if len(line) > 1:
+            line = line.split(',')
+            ts.append(float(line[0]) / 2)
+            xs.append(-float(line[1]))
+            if float(line[0]) >= 80:
+                break
+
+    return ts, xs
+
+
+def process_data(ts: List[float], xs: List[float], window: int = 300) -> pd.Series:
     """
-    Function which is called to plot the data.
+    Process the data with a rolling window standard deviation.
 
     Args:
-        i: int. Frame number.
+        ts (List[float]): The time-series data.
+        xs (List[float]): The measurement data.
+        window (int): The rolling window size. Defaults to 300.
+
+    Returns:
+        pd.Series: The processed data.
     """
-    ax1.clear()
-    colors = ['-b', '-r', '-y', '-g', '-p', '-c', '-m', '-w', '-rs', '-gs']
-    for file_path, color, psi in zip(file_paths, colors, range(10, 110, 10)):
-        ts, data = process_file(file_path)
-        ax1.plot(ts, data, color, label=f'{psi} PSI')
+    logging.debug('Processing data')
+    xs_series = pd.Series(xs)
+    data = xs_series.rolling(window).std()
 
-    ax1.legend(loc='upper left')
-    ax1.set_title('Comparison of pulse and no pulse data at 50 PSI Tall Whisker')
-    ax1.set_xlabel('Time(s)')
-    ax1.set_ylabel('MWSD of Raw Voltage')
+    return data
 
-# Start the animation
-ani1 = animation.FuncAnimation(fig1, animate, interval=30)
-plt.show()
+
+def animate(i, ax, file_paths: List[str], labels: List[str]):
+    """
+    Update the plot for each frame of the animation.
+
+    Args:
+        i (int): The current frame index.
+        ax (object): The matplotlib axes object.
+        file_paths (List[str]): The list of file paths to process.
+        labels (List[str]): The labels for each line in the plot.
+    """
+    ax.clear()
+
+    for file_path, label in zip(file_paths, labels):
+        ts, xs = load_data_from_file(file_path)
+        data = process_data(ts, xs)
+        ax.plot(ts, data, label=label)
+
+    ax.legend(loc='upper left')
+    ax.set_title('Comparison of pulse and no pulse data at 50 PSI Tall Whisker')
+    ax.set_xlabel('Time(s)')
+    ax.set_ylabel('MWSD of Raw Voltage')
+
+
+def main():
+    # Set up the figure and axes
+    fig, ax = plt.subplots()
+
+    # Define the file paths and labels for each data file
+    base_path = "D:/SMP/OneDrive - Singapore University of Technology and Design/Whiskers Array Sensing/191001/Stiff Whisker 2 cm/"
+    file_names = [f"{i * 10} PSI Sweep_1.txt" for i in range(1, 11)]
+    file_paths = [os.path.join(base_path, file_name) for file_name in file_names]
+    labels = [f'{i * 10} PSI' for i in range(1, 11)]
+
+    ani = animation.FuncAnimation(fig, animate, interval=30, fargs=(ax, file_paths, labels))
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
